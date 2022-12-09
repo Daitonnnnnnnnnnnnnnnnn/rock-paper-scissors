f6d91d8d8709ea698f34889db52752f960145550
1oL <114190398+Daitonnnnnnnnnnnnnnnnn@users.noreply.github.com>
2022-12-09 13:51:53 -0800

    Add files via upload

---
+++ Rock Paper Scissors game.py
@@ -0,0 +1,199 @@
+import random
+
+user_action = input("Enter a choice (rock, paper, scissors, gun, large schlong, tsar bomba: ")
+possible_actions = ["rock", "paper", "scissors", "gun", "large schlong", "tsar bomba"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
+if user_action == "gun":
+    if computer_action == "paper":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "rock":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "large schlong":
+        print("You shot off the oponents large schlong. You win!")
+if user_action == "gun":
+    if computer_action == "scissors":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "large schlong":
+    if computer_action == "scissors":
+        print("*slice* Your large schlong is no longer attached. You lose!")
+if user_action == "large schlong":
+    if computer_action == "rock":
+        print("Your schlong has been crushed. You lose.")
+if user_action == "large schlong":
+    if computer_action == "paper":
+        print("Your large schlong has suffered a paper-cut. You have died of blood loss. You lose!")
+if user_action == "large schlong":
+    if computer_action == "gun":
+        print("Your large schlong has been shot off. You lose.")
+if user_action == "gun":
+    if computer_action == "tsar bomba":
+        print("Your 9mm gun has punctured the mighty tsar bomba. It exploded. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "rock":
+        print("The rock has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "paper":
+        print("The paper has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "scissors":
+        print("The scissors have disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "gun":
+        print("The opponent has shot your tsar bomba. They are evaporated. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "large schlong":
+        print("Opponents swings their large schlong, hitting away your tsar bomba like a baseball. You lose.")
+import random
+
+user_action = input("Enter a choice (rock, paper, scissors, gun, large schlong, tsar bomba: ")
+possible_actions = ["rock", "paper", "scissors", "gun", "large schlong", "tsar bomba"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
+if user_action == "gun":
+    if computer_action == "paper":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "rock":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "large schlong":
+        print("You shot off the oponents large schlong. You win!")
+if user_action == "gun":
+    if computer_action == "scissors":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "large schlong":
+    if computer_action == "scissors":
+        print("*slice* Your large schlong is no longer attached. You lose!")
+if user_action == "large schlong":
+    if computer_action == "rock":
+        print("Your schlong has been crushed. You lose.")
+if user_action == "large schlong":
+    if computer_action == "paper":
+        print("Your large schlong has suffered a paper-cut. You have died of blood loss. You lose!")
+if user_action == "large schlong":
+    if computer_action == "gun":
+        print("Your large schlong has been shot off. You lose.")
+if user_action == "gun":
+    if computer_action == "tsar bomba":
+        print("Your 9mm gun has punctured the mighty tsar bomba. It exploded. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "rock":
+        print("The rock has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "paper":
+        print("The paper has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "scissors":
+        print("The scissors have disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "gun":
+        print("The opponent has shot your tsar bomba. They are evaporated. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "large schlong":
+        print("Opponents swings their large schlong, hitting away your tsar bomba like a baseball. You lose.")
+import random
+
+user_action = input("Enter a choice (rock, paper, scissors, gun, large schlong, tsar bomba: ")
+possible_actions = ["rock", "paper", "scissors", "gun", "large schlong", "tsar bomba"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
+if user_action == "gun":
+    if computer_action == "paper":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "rock":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "gun":
+    if computer_action == "large schlong":
+        print("You shot off the oponents large schlong. You win!")
+if user_action == "gun":
+    if computer_action == "scissors":
+        print("Opponent has taken an L + skill issue. You win!")
+if user_action == "large schlong":
+    if computer_action == "scissors":
+        print("*slice* Your large schlong is no longer attached. You lose!")
+if user_action == "large schlong":
+    if computer_action == "rock":
+        print("Your schlong has been crushed. You lose.")
+if user_action == "large schlong":
+    if computer_action == "paper":
+        print("Your large schlong has suffered a paper-cut. You have died of blood loss. You lose!")
+if user_action == "large schlong":
+    if computer_action == "gun":
+        print("Your large schlong has been shot off. You lose.")
+if user_action == "gun":
+    if computer_action == "tsar bomba":
+        print("Your 9mm gun has punctured the mighty tsar bomba. It exploded. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "rock":
+        print("The rock has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "paper":
+        print("The paper has disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "scissors":
+        print("The scissors have disappeared, as well as everybody In a 62 miles radius. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "gun":
+        print("The opponent has shot your tsar bomba. They are evaporated. Nobody wins.")
+if user_action == "tsar bomba":
+    if computer_action == "large schlong":
+        print("Opponents swings their large schlong, hitting away your tsar bomba like a baseball. You lose.")
+
